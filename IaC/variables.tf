bf236bd69ea84b63623d1694b2d54516f3153407
Adeola Ibrahim <66420961+Ayanfe5@users.noreply.github.com>
2023-06-06 12:22:41 +0100

    Create variables.tf

---
+++ IaC/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+}
