25c282341118416e2055b1478bcffb7a8c922646
Adeola Ibrahim <66420961+Ayanfe5@users.noreply.github.com>
2023-06-06 12:23:19 +0100

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS secret access key"
-  type        = string
-}
