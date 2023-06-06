45a31d3a9a5fbe2e2301adcef06d8275e8097a53
Adeola Ibrahim <66420961+Ayanfe5@users.noreply.github.com>
2023-06-06 12:27:38 +0100

    Update main.tf

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
- access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
- secret_access_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
- region = "us-west-1"
-}
-
 resource "aws_instance" "appsilon-task" {
   ami           = "ami-0f8e81a3da6e2510a"  # Ubuntu 22.04 LTS
   instance_type = "t2.micro"
