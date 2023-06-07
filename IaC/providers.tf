9f0fb9af74ec9e72815f1811be731f1c96062f3c
Adeola Ibrahim <66420961+Ayanfe5@users.noreply.github.com>
2023-06-07 10:24:31 +0100

    Update providers.tf

--- IaC/providers.tf
+++ IaC/providers.tf
@@ -17,8 +17,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
   region     = "us-west-1"
   
   features {}
